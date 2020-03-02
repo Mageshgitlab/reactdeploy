3069d45ace6f06f47fbeec56fadeaca4d8a87a8c
Magesh Sekar <mageshsaa@gmail.com>
2020-03-02 17:15:00 +0530

    Updates

---
+++ precache-manifest.ce86ba137cfe2e34eb69ffd751113de3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6443feba1ed622bda7c1edff16160986",
+    "url": "/reactdeploy/index.html"
+  },
+  {
+    "revision": "328a61f91b6acd433543",
+    "url": "/reactdeploy/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "468891a1d88a58679dd3",
+    "url": "/reactdeploy/static/js/2.de55fec7.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/reactdeploy/static/js/2.de55fec7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "328a61f91b6acd433543",
+    "url": "/reactdeploy/static/js/main.f058a034.chunk.js"
+  },
+  {
+    "revision": "5841fe688ea537282403",
+    "url": "/reactdeploy/static/js/runtime-main.49bdba62.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactdeploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
